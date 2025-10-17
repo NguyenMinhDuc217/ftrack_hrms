2347a6994ba9a6955cde724b71903128344d7bc6
ninhhuynh <ninh.huynh@cpm-vietnam.com>
2025-10-17 09:45:44 +0700

    dev: add ide_helper, docs generate

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $department_id
+ * @property string|null $department_name
+ * @property string|null $description
+ * @property string|null $type
+ * @property string|null $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereDepartmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereDepartmentName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Department whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Department extends Model
 {
     protected $fillable = [
